6726901217f20af045eb23ea14eae7de6b75375c
lasith8 <lasithdulshan62@gmail.com>
2026-07-24 12:15:08 +0530

    Update website

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-orange-500">
+        LAKAFX ADMIN DASHBOARD
+      </h1>
+    </div>
+  );
+}
